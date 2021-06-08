ab7ff2b122076f2d5a7d949fa093b219f7634da1
ArielBlG <62558598+ArielBlG@users.noreply.github.com>
2021-06-08 20:06:00 +0300

    fix input tests

--- project/Domain/FIFARep.js
+++ project/Domain/FIFARep.js
@@ -26,53 +26,79 @@ async function addGameToSystem({ body: { homeTeam, awayTeam, mainRef, assRef1, a
     main_ref = await RefObj.getReferee(mainRef);
     assistant_ref_1 = await RefObj.getReferee(assRef1);
     assistant_ref_2 = await RefObj.getReferee(assRef2);
-    if (main_ref.Role !== "Main" || assistant_ref_1.Role !== "Assistent" || assistant_ref_2.Role !== "Assistent") {
-        throw { status: 400, message: "Referee Role Doesn't fit" };
-    }
-    // check if teams and references exists in db
-    if (homeTeam && awayTeam && main_ref && assistant_ref_1 && assistant_ref_2) {
-        //check if game id already in DB
-        let output_id = await DButils.execQuery(
-            `
-            SELECT game_id from dbo.Games
-            WHERE game_id = '${id}' 
-            `
-        )
-        //checks if a team is playing this date
-        let output_team_games = await DButils.execQuery(
-            `
-            SELECT * from dbo.Games
-            WHERE (HomeTeam = '${homeTeam}' AND Time = '${date}')
-                    or (awayTeam ='${awayTeam}' AND time  = '${date}')
 
-            `
-        )
-        //check if the referees are having games at the same timeout
-        let output_ref_games = await DButils.execQuery(
-            `
-            SELECT * from dbo.Games
-            WHERE (Time= '${date}' AND
-             (MainReferee = '${mainRef}' OR AssistantReferee1 ='${assRef1}' OR AssistantReferee2 ='${assRef2}'
-             OR AssistantReferee1 ='${assRef2}' OR AssistantReferee2 ='${assRef1}'))
+    // Checks if one of the teams doesn't exists in the system
+    if (homeTeam){
+        throw { status: 400, message: "The Home Team Doesn't exists in the system." };
+    }
+    if (awayTeam){
+        throw { status: 400, message: "The Away Team Doesn't exists in the system." };
+    }
 
-            `
-        )
-        //checks if the game is already in db.
-        if (output_id.length == 0 && output_team_games.length == 0 && output_ref_games.length == 0) {
-            await DButils.execQuery(
-                `insert into dbo.Games
-                (Time, HomeTeam, AwayTeam, Stadium, game_id, MainReferee, AssistantReferee1, AssistantReferee2, league, stage )
-                VALUES
-                ('${date}','${homeTeam}','${awayTeam}','${stadium}','${id}', '${mainRef}','${assRef1}', '${assRef2}', '${league}', '${stage}' )`
-            );
-            // return 200;
-        } else {
-            throw { status: 400, message: "Team or Referee already has game this time" };
-        }
+    //Checks if one of the referees doesn't exists in the system
+    if(mainRef){
+        throw {status: 400, message: "The Main Referee doesn't exists in the system."};
     }
-    else {
-        throw { status: 400, message: "Team or Referee doesn't exists in the system" };
+    if(assistant_ref_1){
+        throw {status: 400, message: "The first assistant Referee doesn't exists in the system."};
     }
+    if(assistant_ref_2){
+        throw {status: 400, message: "The second assistan Referee doesn't exists in the system."};
+    }
+
+    //Checks if the referee's Role matches the role that was recevied
+    if (main_ref.Role !== "Main"){
+        throw { status: 400, message: "Main Referee Role Doesn't fit" };
+    }
+    if(assistant_ref_1.Role !== "Assistent"){
+        throw {status: 400, message: "The first assistan Referee's Role doesn't fit."};
+    }
+    if(assistant_ref_2.Role !== "Assistent"){
+        throw {status: 400, message: "The second assistan Referee's Role doesn't fit."};
+    }
+    
+    //check if theres a game already with the recevied id
+    let output_id = await DButils.execQuery(
+        `
+        SELECT game_id from dbo.Games
+        WHERE game_id = '${id}' 
+        `
+    )
+    if(output_id.length !== 0){
+        throw {status: 400, message: "The Game ID already exists in the system"}
+    }
+    //checks if a team is playing this date
+    let output_team_games = await DButils.execQuery(
+        `
+        SELECT * from dbo.Games
+        WHERE (HomeTeam = '${homeTeam}' AND Time = '${date}')
+                or (awayTeam ='${awayTeam}' AND time  = '${date}')
+
+        `
+    )
+    if(output_team_games.length !== 0){
+        throw {status: 400, message: "One of the teams has game in this Date"};
+    }
+    //check if the referees are having games at the same timeout
+    let output_ref_games = await DButils.execQuery(
+        `
+        SELECT * from dbo.Games
+        WHERE (Time= '${date}' AND
+            (MainReferee = '${mainRef}' OR AssistantReferee1 ='${assRef1}' OR AssistantReferee2 ='${assRef2}'
+            OR AssistantReferee1 ='${assRef2}' OR AssistantReferee2 ='${assRef1}'))
+
+        `
+    )
+    if(output_ref_games.length !== 0){
+        throw {status: 400, message: "One of the Referees has game in this Date"};
+    }
+    await DButils.execQuery(
+        `insert into dbo.Games
+        (Time, HomeTeam, AwayTeam, Stadium, game_id, MainReferee, AssistantReferee1, AssistantReferee2, league, stage )
+        VALUES
+        ('${date}','${homeTeam}','${awayTeam}','${stadium}','${id}', '${mainRef}','${assRef1}', '${assRef2}', '${league}', '${stage}' )`
+    );
+
 }
 /**
  * The method that is responsibles to add referee and varify the details
